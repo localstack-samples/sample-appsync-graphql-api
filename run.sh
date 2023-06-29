a8d8c1210f7073a844e65e496bb7ce192d6a6959
Bernhard Matyas <90144234+baermat@users.noreply.github.com>
2023-06-29 08:37:23 +0200

    add smoke test (#5)
    * add simple check
    
    * add check for second integration

--- run.sh
+++ run.sh
@@ -16,7 +16,15 @@ echo "Now trying to invoke the AppSync API for DynamoDB integration under $APPSY
 curl -H "Content-Type: application/json" -H "x-api-key: $api_key" -d '{"query":"mutation {addPostDDB(id: \"id123\"){id}}"}' $APPSYNC_URL/$api_id && \
 curl -H "Content-Type: application/json" -H "x-api-key: $api_key" -d '{"query":"query {getPostsDDB{id}}"}' $APPSYNC_URL/$api_id && \
 echo "Scanning items from DynamoDB table - should include entry with 'id123':" && \
-awslocal dynamodb scan --table-name table1 && \
+result_ddb_scan=$(awslocal dynamodb scan --table-name table1) && \
+echo $result_ddb_scan | jq -r . && \
 echo "Now trying to invoke the AppSync API for RDS integration." && \
 curl -H "Content-Type: application/json" -H "x-api-key: $api_key" -d '{"query":"mutation {addPostRDS(id: \"id123\"){id}}"}' $APPSYNC_URL/$api_id && \
-curl -H "Content-Type: application/json" -H "x-api-key: $api_key" -d '{"query":"query {getPostsRDS{id}}"}' $APPSYNC_URL/$api_id
+result_rds=$(curl -H "Content-Type: application/json" -H "x-api-key: $api_key" -d '{"query":"query {getPostsRDS{id}}"}' $APPSYNC_URL/$api_id) && \
+echo $result_rds
+expected_id_rds=$(echo $result_rds | jq -r .data.getPostsRDS[0].id)
+expected_id_ddb=$(echo $result_ddb_scan | jq -r .Items[0].id.S)
+if [[ $expected_id_rds != "id123" ]] || [[ $expected_id_ddb != "id123" ]]; then
+    echo unexpected id query results: $expected_id_rds, $expected_id_ddb
+    exit 1
+fi
